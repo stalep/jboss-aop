8958033f3dafff9c35c3cec0507d2c82773eb988
flavia.rainone@jboss.com <flavia.rainone@jboss.com@84be2c1e-ba19-0410-b317-a758671a6fc1>
2007-05-02 21:13:23 +0000

    Improved the format of no such advice error message.
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/aop/trunk@62762 84be2c1e-ba19-0410-b317-a758671a6fc1

--- aop/src/main/org/jboss/aop/AspectXmlLoader.java
+++ aop/src/main/org/jboss/aop/AspectXmlLoader.java
@@ -1,24 +1,24 @@
 /*
-  * JBoss, Home of Professional Open Source
-  * Copyright 2005, JBoss Inc., and individual contributors as indicated
-  * by the @authors tag. See the copyright.txt in the distribution for a
-  * full listing of individual contributors.
-  *
-  * This is free software; you can redistribute it and/or modify it
-  * under the terms of the GNU Lesser General Public License as
-  * published by the Free Software Foundation; either version 2.1 of
-  * the License, or (at your option) any later version.
-  *
-  * This software is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  * Lesser General Public License for more details.
-  *
-  * You should have received a copy of the GNU Lesser General Public
-  * License along with this software; if not, write to the Free
-  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
-  */
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.aop;
 
 import java.io.IOException;
@@ -385,15 +385,15 @@ public class AspectXmlLoader implements XmlLoader
                   catch (IllegalArgumentException e)
                   {
                      StringBuffer message = new StringBuffer();
-                     message.append("unexpected tag inside binding:");
+                     message.append("unexpected tag inside binding: \'");
                      message.append(tag2);
-                     message.append("\n should be one of: \'interceptor\', \'interceptor-ref\', \'advice\', \'");
-                     for(AdviceType adviceType: AdviceType.values())
+                     message.append("\'\n\t\t\t\t\t   should be one of: \n\t\t\t\t\t\t'interceptor\', \n\t\t\t\t\t\t'interceptor-ref\', \n\t\t\t\t\t\t'advice\', \n\t\t\t\t\t\t'");
+                     for (AdviceType adviceType : AdviceType.values())
                      {
                         message.append(adviceType.getDescription());
-                        message.append("\', \'");
+                        message.append("\', \n\t\t\t\t\t\t'");
                      }
-                     message.append("stack-ref\'");
+                     message.append("stack-ref\'.\n");
                      throw new RuntimeException(message.toString());
                   }
                }
