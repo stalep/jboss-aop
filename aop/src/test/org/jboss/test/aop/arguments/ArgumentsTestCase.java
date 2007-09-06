996227d088adce76417f52c92f22448bb64abbd5
flavia.rainone@jboss.com <flavia.rainone@jboss.com@84be2c1e-ba19-0410-b317-a758671a6fc1>
2007-09-06 19:33:32 +0000

    [JBAOP-382] First step of refactoring: rename args package.
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/aop/trunk@65192 84be2c1e-ba19-0410-b317-a758671a6fc1

--- aop/src/test/org/jboss/test/aop/arguments/ArgumentsTestCase.java
+++ aop/src/test/org/jboss/test/aop/arguments/ArgumentsTestCase.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.test.aop.args;
+package org.jboss.test.aop.arguments;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
