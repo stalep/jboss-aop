c28e6108b7962b301c53428b54d472e99430af89
stalep <stalep@84be2c1e-ba19-0410-b317-a758671a6fc1>
2008-03-31 15:22:59 +0000

    [JBAOP-547] updated the build files with the new mc dependencies
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/aop/trunk@71485 84be2c1e-ba19-0410-b317-a758671a6fc1

--- aop/src/resources/bin/run-load15HotSwap.bat
+++ aop/src/resources/bin/run-load15HotSwap.bat
@@ -44,7 +44,8 @@ goto setupArgs
 REM Setup AOP classpath
 SET AOPC_CLASSPATH=..\lib-50\javassist.jar
 SET AOPC_CLASSPATH=%AOPC_CLASSPATH%;..\lib-50\jboss-aop-jdk50-client.jar
-SET AOPC_CLASSPATH=%AOPC_CLASSPATH%;..\lib-50\jboss-container.jar
+SET AOPC_CLASSPATH=%AOPC_CLASSPATH%;..\lib-50\jboss-reflect.jar
+SET AOPC_CLASSPATH=%AOPC_CLASSPATH%;..\lib-50\jboss-mdr.jar
 SET AOPC_CLASSPATH=%AOPC_CLASSPATH%;..\lib-50\jboss-logging-log4j.jar
 SET AOPC_CLASSPATH=%AOPC_CLASSPATH%;..\lib-50\jboss-logging-spi.jar
 SET AOPC_CLASSPATH=%AOPC_CLASSPATH%;..\lib-50\jboss-standalone-aspect-library-jdk50.jar
